3e101badc12b38077de61f70834fbdbd39d9768e
Faith Jesmain <faithjesmain@gmail.com>
2025-04-26 11:23:22 -0400

    Quick Update
    just putting it on github

---
+++ stringproject.py
@@ -0,0 +1,15 @@
+# Convert a list of characters into a string
+
+# List of characters
+characters = ["P", "Y", "T", "H", "O", "N"]
+
+# Join characters
+string1 = "".join(characters)
+print(string1.title())
+
+# Convert string to list of characters
+string2 = "PYTHON"
+
+# Separate
+list1 = list(string2)
+print(list1)
